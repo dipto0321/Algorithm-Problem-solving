19ff46932983c656f0c05264f4e3b86c4c7790fc
Dipto Karmakar <diptokmk47@gmail.com>
2024-10-26 00:51:39 +0600

    Week 1 (#1)
    * feat: solved codeware problem name narcissistic
    
    * refactor: Refactor narcissistic code
    
    * feat: solved codeware problem name duplicate_encode
    
    * chore(version): 0.1.0

---
+++ narcissistic.py
@@ -0,0 +1,18 @@
+def narcissistic(value):
+    """
+    Check if a number is a narcissistic number.
+
+    A narcissistic (or Armstrong) number is a number that is the sum of its own digits each raised to the power of the number of digits.
+
+    Args:
+        value (int): The number to check.
+
+    Returns:
+        bool: True if the number is narcissistic, False otherwise.
+    """
+    digits = [int(x) for x in str(value)]
+    digit_len = len(digits)
+    return sum(digit**digit_len for digit in digits) == value
+
+
+print(narcissistic(372))
